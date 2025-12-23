3b51b6c8b98c051b0058f63808b19536fad73239
KavyaSunithaVijayan <kavyavijayan6713@gmail.com>
2025-12-23 09:52:12 +0530

    Feature: New extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
-import { GetWeather } from "./lib/api";
+import { useState, useEffect } from "react";
+import { GetForecast, GetWeather } from "./lib/api";
 import { BsSearch } from "react-icons/bs";
 import Lottie from "lottie-react";
 import wind from "./lib/animation/wind.json";
+import sun from "./lib/animation/sun.json";
 import humidity from "./lib/animation/humidity.json";
 import empty_query from "./lib/animation/empty_query.json";
 import loader from "./lib/animation/loader.json";
 import no_data from "./lib/animation/no_data.json";
+import "./App.css";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [weatherData, setWeatherData] = useState("");
+  const [weatherImage, setWeatherImage] = useState("");
+  const [weatherForecast, setWeatherForecast] = useState("");
+  const [showFahrenheit, setShowFahrenheit] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [error, setError] = useState(false);
   const handleSubmit = async () => {
     if (!inputText.trim()) return;
     setLoading(true);
     try {
       const response = await GetWeather(inputText);
-      setWeatherData(response?.data);
+      setWeatherData(response);
+      setWeatherImage(
+        `http://openweathermap.org/img/wn/${response?.weather[0]?.icon}@2x.png`
+      );
+      if (response) {
+        setError(false);
+        const weatherResponse = await GetForecast(
+          response.coord.lat,
+          response.coord.lon
+        );
+        setWeatherForecast(weatherResponse);
+      }
     } catch (error) {
-      console.error("Error fetching weather data", error);
+      setError(true);
     } finally {
       setLoading(false);
+      setInputText("");
     }
   };
+
   const getCurrentDate = () => {
     const options = {
       weekday: "long",
@@ -35,8 +54,47 @@ function App() {
     const currentDate = new Date().toLocaleDateString("en-US", options);
     return currentDate;
   };
+  const toggleTemperature = () => {
+    setShowFahrenheit((prev) => !prev);
+  };
+  const toCelsius = (f) => ((f - 32) * 5) / 9;
+
+  const temperature = showFahrenheit
+    ? `${weatherData?.main?.temp.toFixed(2)}°F`
+    : `${toCelsius(weatherData?.main?.temp).toFixed(2)}°C`;
+
+  const convertUnixToTime = (unixTimestamp, timeZone = "Asia/Kolkata") => {
+    if (!unixTimestamp) return "-";
+    return new Intl.DateTimeFormat("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone,
+    }).format(new Date(unixTimestamp * 1000));
+  };
+
+  const sunriseTime = convertUnixToTime(
+    weatherData?.sys?.sunrise,
+    "Asia/Kolkata"
+  );
+  const sunsetTime = convertUnixToTime(
+    weatherData?.sys?.sunset,
+    "Asia/Kolkata"
+  );
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setInitialLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="py-5">
+    <div className="py-5 max-h-screen overflow-auto">
+      {initialLoading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white loader_body">
+          <h2 className="loader_text">SkyMate</h2>
+        </div>
+      )}
       {loading && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(255,255,255,0.4)]">
           <Lottie animationData={loader} loop autoplay className="w-64 h-64" />
@@ -48,8 +106,14 @@ function App() {
             <input
               type="text"
               placeholder="Enter city name"
+              value={inputText}
               className="bg-white text-sm rounded-lg w-full p-2 focus:outline-none"
               onChange={(e) => setInputText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSubmit();
+                }
+              }}
             />
           </div>
           <button
@@ -70,7 +134,7 @@ function App() {
             />
             <p className="mt-2 italic text-sm">Search a location!</p>
           </div>
-        ) : weatherData?.success === false ? (
+        ) : error ? (
           <div className="py-10">
             <Lottie
               animationData={no_data}
@@ -84,32 +148,36 @@ function App() {
           </div>
         ) : (
           <div>
-            <div className="py-5">
+            <div className="py-5 ">
               <span className="flex text-2xl font-semibold justify-center">
-                {weatherData?.request?.query}
+                {weatherData?.name},{"  "}
+                {weatherData?.sys?.country}
               </span>
             </div>
             <span className="flex text-sm italic justify-center">
               {getCurrentDate()}
             </span>
-            <div className="flex gap-10 pt-7 pb-5 justify-center">
+            <div className="flex gap-10 pt-4 sm:pt-7 justify-center">
               <div className="flex gap-5">
-                <img
-                  src={weatherData?.current?.weather_icons}
-                  className="rounded-xl"
-                />
                 <div className="flex gap-2">
-                  <span className="font-bold text-6xl">
-                    {weatherData?.current?.temperature | 0}
+                  <span className="flex justify-center font-semibold items-center">
+                    {weatherImage ? (
+                      <img src={weatherImage} alt="weather" />
+                    ) : null}
                   </span>
-                  <span className="font-semibold text-lg">°C</span>
+                  <button
+                    className="font-bold text-2xl sm:text-6xl text-blue-500"
+                    onClick={toggleTemperature}
+                  >
+                    {temperature || 0}
+                  </button>
                 </div>
               </div>
             </div>
-            <span className="flex justify-center text-[#F06792] font-semibold">
-              {weatherData?.current?.weather_descriptions}
+            <span className="text-lg flex justify-center ">
+              {weatherData?.weather[0]?.description}
             </span>
-            <div className="mx-15 pt-10 flex items-center gap-10 sm:gap-20 justify-center">
+            <div className="mx-0 sm:mx-15 pt-5 flex items-center gap-10 sm:gap-20 justify-center">
               <div className="flex items-center">
                 <Lottie
                   animationData={wind}
@@ -119,7 +187,7 @@ function App() {
                 />
                 <div>
                   <span className="text-[15px]">
-                    {weatherData?.current?.wind_speed} <span>m/s</span>
+                    {weatherData?.wind?.speed} m/s
                   </span>
                   <h2 className="text-[10px] text-center">Wind speed</h2>
                 </div>
@@ -133,15 +201,29 @@ function App() {
                 />
                 <div>
                   <span className="text-[15px]">
-                    {weatherData?.current?.humidity} <span>%</span>
+                    {weatherData?.main?.humidity} <span>%</span>
                   </span>
                   <h2 className="text-[10px] text-center">Humidity</h2>
                 </div>
               </div>
             </div>
+            <div className="mx-0 sm:mx-15 flex items-center gap-10 sm:gap-20 justify-center">
+              <Lottie
+                animationData={sun}
+                loop={true}
+                autoplay={true}
+                className="w-1/2 h-1/2"
+              />
+            </div>
+            <span className="text-[10px]">{sunriseTime}</span> -{" "}
+            <span className="text-[10px]">{sunsetTime}</span>
           </div>
         )}
       </div>
+      <p className="fixed bottom-0 italic w-full text-center text-xs sm opacity-50 ">
+        Designed and developer by{" "}
+        <span className="text-pink-400">Kavya Vijayan</span>
+      </p>
     </div>
   );
 }
--- src/lib/api/index.tsx
+++ src/lib/api/index.tsx
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 export const GetWeather = async (city: any) => {
-  const GetWeatherUrl = `https://api.weatherstack.com/current?access_key=4cb148a4bba8b254bed060d5939c9fa7&query=${city}`;
-  const response = axios.get(GetWeatherUrl);
-  return response;
+  const GetWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appId=f785b82226f9466fa0834e8f6e891c6c`;
+  const response = await axios.get(GetWeatherUrl);
+  return response?.data;
+};
+export const GetForecast = async (lat: any, lon: any) => {
+  const GetForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appId=f785b82226f9466fa0834e8f6e891c6c`;
+  const response = await axios.get(GetForecastUrl);
+  return response?.data;
 };
